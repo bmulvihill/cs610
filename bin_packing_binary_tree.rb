7f2b29669eec9bbc058ffadcc58e968cdef55f5b
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-06-18 21:12:17 -0400

    update bin packing

--- bin_packing_binary_tree.rb
+++ bin_packing_binary_tree.rb
@@ -1,39 +1,40 @@
 module BinaryTree
   class Node
-    attr_accessor :left, :right, :parent, :value, :size
+    include Comparable
+    attr_accessor :left, :right, :parent, :value
+    
     def initialize(value)
       @value = value
-      @size = 1.0
+    end
+
+    def <=>(otherNode)
+      @value <=> otherNode.value
     end
   end
 
   class BinaryTree
-    attr_accessor :root, :nodes
+    attr_accessor :root
     
-    def initialize(root)
+    def initialize(root = nil)
       @root = root
-      @nodes =[]
     end
 
-    def insert_node(root, node)
-      if node.value < root.value
-        return root.left = node, node.parent = root if root.left.nil?
-        return insert_node(root.left,node)
-      else
-        return root.right = node, node.parent = root if root.right.nil?
-        return insert_node(root.right, node)
-      end
+    def search(value)
+      search_for_node(@root,Node.new(value))
     end
 
-    def delete_node(root, node)
-      if root.value == node.value
-         root = remove(root)
-      elsif node.value < root.value
-         root.left = delete_node(root.left,node)
-      else
-         root.right = delete_node(root.right,node)
-      end
-      root
+    def insert(value)
+      @root = insert_value(@root,value)
+    end
+
+    def delete(value)
+      @root = delete_node(@root,Node.new(value))
+    end
+
+    def in_order_list
+        vals = []
+         inorder(vals,@root)
+        vals
     end
 
     def traverse_tree(node)
@@ -44,84 +45,103 @@ module BinaryTree
       end
     end
 
-    def set_nodes(node, weight)
+    def get_nodes(container, node, weight)
       unless node.nil?
-        set_nodes(node.left, weight)
-        process_best_fit(node, weight)
-        set_nodes(node.right, weight)
+        get_nodes(container, node.left, weight)
+        left_over = process_best_fit(node, weight)
+        return container.push(left_over) unless left_over.nil?
+        get_nodes(container, node.right, weight)
       end
     end
 
     def process_best_fit(node, weight)
-      if node.size - weight.weight >= 0
-        @nodes << node
+      if node.value - weight.weight >= 0
+        node
       end
     end
 
     def process_item(node)
-      puts node.size
+      puts node.value
     end
+    
+private
+  def search_for_node(root,node)
+      if root.nil?
+         return nil
+      end
 
-    def search_tree(node, item)
-      return if node.nil?
-      return node if node.value == item
-      if item < node.value
-        return search_tree(node.left, item)
+      if root == node
+         root = node
+      elsif node < root
+         root = search_for_node(root.left,node)
       else
-        return search_tree(node.right, item)
+         root = search_for_node(root.right,node)
       end
+     root
+  end
+
+  def insert_value(root,value)
+    if root.nil?
+      root = Node.new(value)
+    elsif value < root.value               
+      root.left = insert_value(root.left,value)          
+    else
+      root.right = insert_value(root.right,value)
     end
-    
-    def find_minimum
-      return if @root.nil?
-      min = @root
-      while min.left != nil
-        min = min.left
+   root
+  end
+
+ def delete_node(root,node)
+      if root == node
+         root = remove(root)
+      elsif node < root
+         root.left = delete_node(root.left,node)
+      else
+         root.right = delete_node(root.right,node)
       end
-      return min
+     root
+  end
+
+  def remove(node)
+    if node.left.nil? && node.right.nil?
+       node = nil
+    elsif !node.left.nil? && node.right.nil?
+       node = node.left
+    elsif node.left.nil? && !node.right.nil?
+       node = node.right
+    else
+       node = replace_parent(node)
     end
+   node
+  end
 
-    def find_maximum
-      return if @root.nil?
-      max = @root
-      while max.right != nil
-        max = max.right
+ def replace_parent(node)
+       node.value = successor_value(node.right)
+       node.right = update(node.right)
+       node
+  end
+
+  def successor_value(node)
+      unless node.left.nil?
+        successor_value(node.left)
       end
-      return max
+      node.value
+  end
+
+  def update(node)
+      unless node.left.nil?
+        node.left = update(node)
+      end
+      node.right
+  end
+
+  def inorder(list, node)
+    unless node.nil?
+       inorder(list, node.left)
+       list.push(node.value)
+       inorder(list, node.right)
     end
-    
-    private
-      def remove(node)
-        if node.left.nil? && node.right.nil?
-          node = nil
-        elsif !node.left.nil? && node.right.nil?
-          node = node.left
-        elsif node.left.nil? && !node.right.nil?
-          node = node.right
-        else
-          node = replace_parent(node)
-        end
-      end
-
-      def replace_parent(node)
-        node.value = successor_value(node.right)
-        node.right = update(node.right)
-        node
-      end
-
-      def successor_value(node)
-        unless node.left.nil?
-          successor_value(node.left)
-        end
-        node.value
-      end
-
-      def update(node)
-        unless node.left.nil?
-          node.left = update(node)
-        end
-        node.right
-      end
+  end
   end
 
   class Weight
@@ -131,27 +151,35 @@ module BinaryTree
     end
   end
 
-
   class BestFitBinPacking
-    def self.execute
+    def self.execute(weights=[])
+      # TESTING DATA #
       weight1 = Weight.new(0.5)
-      weight2 = Weight.new(0.2)
-      weight3 = Weight.new(1.0)
+      weight2 = Weight.new(0.6)
+      weight3 = Weight.new(0.7)
       weight4 = Weight.new(0.4)
-      weights = [weight1, weight3, weight2, weight4]
-      tree = BinaryTree.new(Node.new(1))
-      bin_count = 1
+      weights = [weight1, weight2, weight4, weight3]
+      # TESTING DATA #
+      tree = BinaryTree.new
+      bin_count = 0
+
       weights.each do |weight|
-        tree.nodes = []
-        tree.set_nodes(tree.root, weight)
-        if tree.nodes == []
+        nodes =[]
+        nodes = tree.get_nodes([], tree.root, weight)
+        if nodes == [] || nodes.nil?
           bin_count += 1
-          tree.insert_node(tree.root, Node.new(2))
-          tree.set_nodes(tree.root, weight)
+          tree.insert(1)
+          nodes = tree.get_nodes([], tree.root, weight)
         end
-        nodes = tree.nodes
-        best_fit = nodes.sort_by {|node| node.size - weight.weight }.first
-        best_fit.size = best_fit.size - weight.weight
+
+        best_fit = nodes.first
+        new_value = best_fit.value - weight.weight
+        if new_value == 0
+          tree.delete(best_fit.value)
+        else
+          tree.delete(best_fit.value)
+          tree.insert(new_value)
+        end
       end
       p "TOTAL BINS: " + bin_count.to_s
       tree.traverse_tree(tree.root)
@@ -159,28 +187,37 @@ module BinaryTree
   end
   
   class WorstFitBinPacking
-    def self.execute
+    def self.execute(weights=[])
+      # TESTING DATA #
       weight1 = Weight.new(0.5)
       weight2 = Weight.new(0.6)
-      weight3 = Weight.new(0.3)
+      weight3 = Weight.new(0.7)
       weight4 = Weight.new(0.4)
-      weights = [weight1, weight3, weight2, weight4]
-      tree = BinaryTree.new(Node.new(1))
-      bin_count = 1
+      weights = [weight1, weight2, weight4, weight3]
+      # TESTING DATA #
+      tree = BinaryTree.new
+      bin_count = 0
+      
       weights.each do |weight|
-        tree.nodes = []
-        tree.set_nodes(tree.root, weight)
-        if tree.nodes == []
+        nodes =[]
+        nodes = tree.get_nodes([], tree.root, weight)
+        if nodes == [] || nodes.nil?
           bin_count += 1
-          tree.insert_node(tree.root, Node.new(2))
-          tree.set_nodes(tree.root, weight)
+          tree.insert(1)
+          nodes = tree.get_nodes([], tree.root, weight)
         end
-        nodes = tree.nodes
-        worst_fit = nodes.sort_by {|node| node.size - weight.weight }.last
-        worst_fit.size = worst_fit.size - weight.weight
+
+        worst_fit = nodes.last
+        new_value = worst_fit.value - weight.weight
+        if new_value == 0
+          tree.delete(worst_fit.value)
+        else
+          tree.delete(worst_fit.value)
+          tree.insert(new_value)
+        end
       end
       p "TOTAL BINS: " + bin_count.to_s
-      tree.traverse_tree(tree.root)       
+      tree.traverse_tree(tree.root)     
     end
   end
 end
