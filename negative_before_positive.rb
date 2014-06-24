6443b682345d1305fb8052bc6e4ba1d046783b08
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-06-23 21:59:24 -0400

    fix spacing

--- negative_before_positive.rb
+++ negative_before_positive.rb
@@ -1,5 +1,5 @@
 class NegativeBeforePositive
-	def self.sort!(keys=[])
+  def self.sort!(keys=[])
     p quick(keys,0,keys.size-1)
   end
  
@@ -7,7 +7,7 @@ class NegativeBeforePositive
  
   def self.quick(keys, left, right)
     if left < right
-    	pivot_index = (left + ((right - left) / 2)).to_i
+      pivot_index = (left + ((right - left) / 2)).to_i
       pivot_index = partition(keys, left, right, pivot_index)
       keys[pivot_index]
       quick(keys, left, pivot_index - 1)
@@ -17,7 +17,7 @@ class NegativeBeforePositive
   end
  
   def self.partition(keys, left, right, pivot)
-   	pivotValue = keys[pivot]
+    pivotValue = keys[pivot]
     keys[pivot], keys[right] = keys[right], keys[pivot]  
     storeIndex = left 
     for i in left..right -1 
