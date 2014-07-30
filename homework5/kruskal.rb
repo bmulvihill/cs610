3e487b2aa62fa4357358c46479d7cfd73f4486f3
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-07-30 19:23:58 -0400

    update kruskals

--- homework5/kruskal.rb
+++ homework5/kruskal.rb
@@ -3,16 +3,15 @@
 
 class UnionFind
   def initialize(n)
-    @leaders = 1.upto(n).inject([]) { |leaders, i| leaders[i] = i; leaders }
+    @forests = 1.upto(n).inject([]) { |forests, i| forests[i] = i; forests }
   end
   
-  def connected?(id1,id2)
-    @leaders[id1] == @leaders[id2]
+  def connected?(vertex1,vertex2)
+    @forests[vertex1] == @forests[vertex2]
   end
   
-  def union(id1,id2)
-    leader_1, leader_2 = @leaders[id1], @leaders[id2]
-    @leaders.map! {|i| (i == leader_1) ? leader_2 : i }
+  def union(vertex1,vertex2)
+    @forests.map! {|i| (i == @forests[vertex1]) ? @forests[vertex2] : i }
   end
 end
 
@@ -29,9 +28,10 @@ edges = [
   {:vertices => [3,4], :weight => -2}
 ].sort_by { |e| e[:weight] }
 
-set = UnionFind.new edges.size
+subset = edges[0..2]
+set = UnionFind.new subset.size
 
-edges.each do |edge|
+subset.each do |edge|
   if !set.connected?(edge[:vertices][0], edge[:vertices][1]) || edge[:weight] < 0
     @minimum_spanning_tree << edge 
     set.union(edge[:vertices][0], edge[:vertices][1])
